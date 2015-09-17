b3ffd924a1cd6733da0525455b5c810e968f76b0
Andhitia Rama <andhitia.r@gmail.com>
2015-09-17 19:01:38 +0700

    Greenify travis

--- l10n_id_account_type/__openerp__.py
+++ l10n_id_account_type/__openerp__.py
@@ -1,26 +1,25 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
+#    Author : Andhitia Rama, Michael Viriyananda, Nurazmi
+#    Copyright (C) 2015 OpenSynergy Indonesia
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2009 P. Christeas <p_christ@hol.gr>. All Rights Reserved
-#    $Id$
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     'name': 'Indonesia - Account Type',
+    'summary' : 'Account type for Indonesia accounting',
     'version': '1.0',
     'author': 'OpenSynergy Indonesia',
     'website': 'www.opensynergy-indonesia.com',
@@ -28,13 +27,7 @@
     'description': """
     """,
     'depends': ['account'],
-    'demo': [],
-    'data': [   
-                        'data/data_AccountType.xml',
-            ],
+    'data': ['data/data_AccountType.xml'],
     'installable': True,
-    'images': [],
 }
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
