31ad962f78042a6da6a23ce857ed1ea1da07bbcc
Andhitia Rama <andhitia.r@gmail.com>
2015-12-01 20:04:13 +0700

    Fix initial import

--- l10n_id_tax_period/objects/__init__.py
+++ l10n_id_tax_period/objects/__init__.py
@@ -18,6 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import tahun_pajak
-import masa_pajak
+import tax_fiscalyear
+import tax_period
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
